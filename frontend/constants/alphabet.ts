3a80be03917cef06f29491b75c26bf83c20c2b03
Kenia Esmeralda Ramos Javier <esme.E_N.tb@outlook.com>
2026-06-09 23:46:37 -0600

    feat: add cons Hands

---
+++ frontend/constants/alphabet.ts
@@ -0,0 +1,78 @@
+
+const HANDS = [
+  {
+    a : require('../../frontend/assets/alfabeto_manos/a.png'),
+  },
+  {
+    b : require('../../frontend/assets/alfabeto_manos/b.png'),
+  },
+  {
+    c : require('../../frontend/assets/alfabeto_manos/c.png'),
+  },
+  {
+    d : require('../../frontend/assets/alfabeto_manos/d.png'),
+  }, 
+  {
+    e : require('../../frontend/assets/alfabeto_manos/e.png'),
+  },
+  {
+    f : require('../../frontend/assets/alfabeto_manos/f.png'),
+  },
+  {
+    g : require('../../frontend/assets/alfabeto_manos/g.png'),
+  },
+  {
+    h : require('../../frontend/assets/alfabeto_manos/h.png'),
+  },
+  {
+    i : require('../../frontend/assets/alfabeto_manos/i.png'),
+  },
+  {
+    j : require('../../frontend/assets/alfabeto_manos/j.png'),
+  },
+  {
+    k : require('../../frontend/assets/alfabeto_manos/k.png'),
+  },
+  {
+    l : require('../../frontend/assets/alfabeto_manos/l.png'),
+  },
+  {
+    m : require('../../frontend/assets/alfabeto_manos/m.png'),
+  },
+  {
+    n : require('../../frontend/assets/alfabeto_manos/n.png'),
+  },
+  {
+    o : require('../../frontend/assets/alfabeto_manos/o.png'),
+  },
+  {
+    p : require('../../frontend/assets/alfabeto_manos/p.png'),
+  },
+  {
+    q : require('../../frontend/assets/alfabeto_manos/q.png'),
+  },
+  {
+    r : require('../../frontend/assets/alfabeto_manos/r.png'),
+  },
+  {
+    s : require('../../frontend/assets/alfabeto_manos/s.png'),
+  },
+  {
+    t : require('../../frontend/assets/alfabeto_manos/t.png'),
+  },
+  {
+    u : require('../../frontend/assets/alfabeto_manos/u.png'),
+  },
+  {
+    v : require('../../frontend/assets/alfabeto_manos/v.png'),
+  },
+  {
+    w : require('../../frontend/assets/alfabeto_manos/w.png'),
+  },
+  {
+    y : require('../../frontend/assets/alfabeto_manos/y.png'),
+  },
+  {
+    z : require('../../frontend/assets/alfabeto_manos/z.png'),
+  },
+]
