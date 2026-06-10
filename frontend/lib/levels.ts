efcfbefcb8acf2c57d578eaf0c6b1136587f8b04
Kenia Esmeralda Ramos Javier <esme.E_N.tb@outlook.com>
2026-06-10 17:45:08 -0600

    feat: add the levels name

---
+++ frontend/lib/levels.ts
@@ -0,0 +1,12 @@
+export const level_rewards = {
+    Newbie : require('../assets/icons/1.Newbie.png'),
+    Beginner : require('../assets/icons/2.Beginner.png'),
+    Apprentice: require('../assets/icons/3.Apprentice.png'),
+    Explorer: require('../assets/icons/4.Explorer.png'),
+    Scholar: require('../assets/icons/5.Scholar.png'),
+    Adept: require('../assets/icons/6.Adept.png'),
+    Expert: require('../assets/icons/7.Expert.png'),
+    Master: require('../assets/icons/8.Master.png'),
+    Grandmaster: require('../assets/icons/9.Grandmaster.png'),
+    Legend: require('../assets/icons/10.Legend.png'),
+} as const
