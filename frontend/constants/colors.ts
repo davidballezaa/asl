5938380292fd89d241dd6d697832d8ab075e538a
Kenia Esmeralda Ramos Javier <esme.E_N.tb@outlook.com>
2026-06-09 20:03:27 -0600

    feat: update colors palets

--- frontend/constants/colors.ts
+++ frontend/constants/colors.ts
@@ -1,16 +1,25 @@
 export const colors = {
-  background: '#FFFAEB',
+  background: '#FFF8E6',
   surface: '#FFFFFF',
-  primary: '#4361EE',
+
+  primary: '#20B8F3',
+  logo: '#20B8F3',
+  blue: '#42C2F5',
+  color_bot: '#009AE6',
+
   secondary: '#F4A261',
   success: '#22C55E',
   error: '#EF4444',
+
   border: '#0F172A',
+
   text: '#0F172A',
   muted: '#475569',
+
   purple: '#8B5CF6',
   cyan: '#06B6D4',
   pink: '#EC4899',
+
   primaryLight: '#E8ECFD',
   successLight: '#DCFCE7',
   errorLight: '#FEE2E2',
